f4ba6c2c83c7330796c01ff9c6a2bb795b84dbee
realsahabia <yakubukarim12@gmail.com>
2023-10-22 22:51:43 +0000

    in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,12 +32,30 @@ export default function Navbar() {
           className="w-96 bg-zinc-900 focus:outline-none border-none"
           />
 
-          <AiOutlineClose className="text-xl cursor-pointer" />
+            <AiOutlineClose className="text-xl cursor-pointer" />
           <button className="h-10 w-16 flex items-center justify-center bg-zinc-800">
             <AiOutlineSearch className="text-xl" />
           </button>
         </div>
       </form>
+      <div className="text-xl p-3 bg-zink-900 rounded-full">
+        <TiMicrophone />
+      </div>
+      <div className="flex gap-5 items-center text-xl">
+        <BsCameraVideo />
+        <IoAppsSharp />
+        <div className="relative">
+          <BsBell />
+          <span className="absolute bottom-2 left-2 text-xs bg-red-600 rounded-full px-1">
+            9+
+          </span>
+        </div>
+        <img 
+        src="https://yt3.ggpht.com/wgnEbc2Ec2JYkeyzUbiHzDlAFObI2Btwo2YRCEF1aCMBiRc5E_zWy8-URBQS3EMQ1yzzaGFR=s88-c-k-c0x00ffffff-no-rj-mo" 
+        alt="logo" 
+        className="w-9 h-9 rounded-full"
+        />
+      </div>
     </div>
   </div>
   )
